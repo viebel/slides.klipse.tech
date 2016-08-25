16e1a4ae1d96fbbfeb96ff2dc39378acbf812b52
Yehonathan Sharvit <viebel@gmail.com>
2016-08-25 13:12:00 +0300

    Update deck.fullscreen.js

--- includes/deckjs/extensions/fullscreen/deck.fullscreen.js
+++ includes/deckjs/extensions/fullscreen/deck.fullscreen.js
@@ -8,63 +8,6 @@ Dual licensed under the MIT license.
 This module adds a fullscreen handler to selected elements.
 */
 (function($, undefined) {
-  var $document = $(document);
-  var rootCounter;
-
-  var updateCurrent = function(event, from, to) {
-    var options = $.deck('getOptions');
-    var currentSlideNumber = to + 1;
-    if (!options.countNested) {
-      currentSlideNumber = $.deck('getSlide', to).data('rootSlide');
-    }
-    $(options.selectors.statusCurrent).text(currentSlideNumber);
-  };
-
-  var markRootSlides = function() {
-    var options = $.deck('getOptions');
-    var slideTest = $.map([
-      options.classes.before,
-      options.classes.previous,
-      options.classes.current,
-      options.classes.next,
-      options.classes.after
-    ], function(el, i) {
-      return '.' + el;
-    }).join(', ');
-
-    rootCounter = 0;
-    $.each($.deck('getSlides'), function(i, $slide) {
-      var $parentSlides = $slide.parentsUntil(
-        options.selectors.container,
-        slideTest
-      );
-
-      if ($parentSlides.length) {
-        $slide.data('rootSlide', $parentSlides.last().data('rootSlide'));
-      }
-      else {
-        ++rootCounter;
-        $slide.data('rootSlide', rootCounter);
-      }
-    });
-  };
-
-  var setInitialSlideNumber = function() {
-    var slides = $.deck('getSlides');
-    var $currentSlide = $.deck('getSlide');
-    var index;
-
-    $.each(slides, function(i, $slide) {
-      if ($slide === $currentSlide) {
-        index = i;
-        return false;
-      }
-    });
-    updateCurrent(null, index, index);
-  };
-
-
-
   var launchFullScreen = function() {
     function launchIntoFullscreen(element) {
       if(element.requestFullscreen) {
@@ -84,9 +27,8 @@ This module adds a fullscreen handler to selected elements.
 
   var attachFullScreen = function() {
     var options = $.deck('getOptions');
-    if (options.fullscreenSelector) {
-      $(options.fullscreenSelector).click(launchFullScreen);
-    }
+    var selector = options.fullscreenSelector || '.fullscreen';
+    $(selector).click(launchFullScreen);
   };
 
   $document.bind('deck.init', function() {
