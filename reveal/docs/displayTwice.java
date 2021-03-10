49292ab410228ae7f0dc7f65237a92982edff713
Yehonathan Sharvit <viebel@gmail.com>
2021-03-10 07:29:15 +0200

    Conf42: Almost done

--- reveal/docs/displayTwice.java
+++ reveal/docs/displayTwice.java
@@ -19,3 +19,44 @@ class MemberBehaviour {
     }
 }
 
+class AuthorData {
+    private String firstName;
+    private String lastName;
+    // setters
+    // getters
+}
+
+class MemberCode {
+    static borrow(MemberData member, String bookId) {
+        if(!member.isBlocked()) {
+            System.out.println("The book is yours!");
+        }
+    }
+}
+
+public record AuthorData (String firstName,
+                          String lastName) {}
+
+class AuthorCode {
+    static String fullName(AuthorData author) {
+        return author.firstName() +
+            " " +
+            author.lastName();
+    }
+}
+
+var isaacAsimov = new AuthorData("Isaac", "Asimov");
+AuthorCode.fullName(isaacAsimov); // "Isaac Asimov"
+
+class AuthorCode {
+    static AuthorData capitalizeLastName(AuthorData author) {
+        author.setFirstName(author.firstName().toUpperCase());
+        return author;
+    }
+}
+
+var asimov = new AuthorData("Isaac", "Asimov");
+var asimov2 = AuthorCode.toUpperLastName(asimov);
+asimov2.lastName() // "ASIMOV"
+asimov.lastName() // ??
+
